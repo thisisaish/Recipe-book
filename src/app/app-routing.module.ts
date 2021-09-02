f9096c985bff6c03c946a4c94011c32b828829e3
AishwaryaMurali <aishumurali98@gmail.com>
2021-09-02 18:24:56 +0530

    Routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe/recipe-start/recipe-start.component";
 
 import { RecipeComponent } from "./recipe/recipe.component";
@@ -9,8 +10,10 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 const appRoutes : Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'}, //only redirects if the full path is empty
     { path: 'recipes', component: RecipeComponent, children: [
-        { path: '', component: RecipeStartComponent},
-        { path: ':id', component: RecipeDetailComponent}
+        { path: '', component: RecipeStartComponent },
+        { path: 'new', component: RecipeEditComponent },
+        { path: ':id', component: RecipeDetailComponent },
+        { path: ':id/edit', component: RecipeEditComponent }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent }
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.compone
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
